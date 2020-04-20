a3cf508c6992a67ab1e622d6395993b0cc91d54e
Blue-Ranger <53351056+Blue-Ranger@users.noreply.github.com>
2020-04-20 18:39:15 -0400

    attempted code for card tab

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,3 +18,60 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/articles")
+  .then((res) => {
+    const ArticleA = res.data.articles.javascript;
+    ArticleA.data = "javascript";
+    const ArticleB = res.data.articles.bootstrap;
+    ArticleB.data = "bootstrap";
+    const ArticleC = res.data.articles.technology;
+    ArticleC.data = "technology";
+    const ArticleD = res.data.articles.jquery;
+    ArticleD.data = "jquery";
+    const ArticleE = res.data.articles.node;
+    ArticleE.data = "node.js";
+
+    const articles = [ArticleA, ArticleB, ArticleC, ArticleD, ArticleE];
+
+    article.forEach((article) => {
+      article.forEach((art) => {
+        const cards = document.querySelector(".cards-container");
+        const card = Article(art);
+
+        card.setAttribute("data-tab", article.data);
+        cards.appendChild(card);
+      });
+    });
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+function Article(obj) {
+  const card = document.createElement("div");
+  const headline = document.createElement("div");
+  const author = document.createElement("div");
+  const imgContainer = document.createElement("div");
+  const img = document.createElement("img");
+  const authorName = document.createElement("span");
+
+  card.classList.add("card");
+  headerline.classList.add("headline");
+  author.classList.add("author");
+  imgContainer.classList.add("img-container");
+
+  headline.textContent = obj.headline;
+  authorName.textContent = obj.authorName;
+  img.src = obj.authorPhoto;
+
+  imgContainer.appendChild(img);
+  author.appendChild(imgContainer);
+  author.appendChild(authorName);
+  card.appendChild(headline);
+  card.appendChild(author);
+
+  return card;
+}
