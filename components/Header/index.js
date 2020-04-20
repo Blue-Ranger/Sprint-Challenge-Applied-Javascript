0ccead368481b646880742c147aadbc125c0d07a
Blue-Ranger <53351056+Blue-Ranger@users.noreply.github.com>
2020-04-20 12:26:57 -0400

    added code for header tab

--- components/Header/index.js
+++ components/Header/index.js
@@ -11,4 +11,32 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+const headerData = {
+  date: "SMARCH 28, 2019",
+  title: "Lambda Times",
+  temp: "98°",
+};
+function Header() {
+  const headerH = document.createElement("div");
+  const headerD = document.createElement("span");
+  const headerL = document.createElement("h1");
+  const headerTemp = document.createElement("span");
+
+  headerH.classList.add("header");
+  headerD.classList.add("date");
+  headerTemp.classList.add("temp");
+
+  headerD.textContent = `${headerData.date}`;
+  headerL.textContent = `${headerData.title}`;
+  headerTemp.textContent = `${headerData.temp}`;
+
+  headerH.appendChild(headerD);
+  headerH.appendChild(headerL);
+  headerH.appendChild(headerTemp);
+
+  return headerH;
+}
+
+const headerContainer = document.querySelector(".header-container");
+
+headerContainer.appendChild(Header());
